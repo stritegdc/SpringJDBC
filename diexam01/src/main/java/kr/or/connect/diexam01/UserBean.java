4ba70d9c7a87efd84612d337718479957fec6e0c
stritegdc <77983074+stritegdc@users.noreply.github.com>
2021-05-21 15:31:26 +0900

    Add files via upload
    Spring Core. IoC/DI 이해.

---
+++ diexam01/src/main/java/kr/or/connect/diexam01/UserBean.java
@@ -0,0 +1,42 @@
+package kr.or.connect.diexam01;
+
+public class UserBean {
+	//1. 기본생성자 2. 필드는 private 3. getter, setter 메소드
+	private String name;
+	private int age;
+	private boolean male;
+	
+	public UserBean() {}
+	
+	public UserBean(String name, int age, boolean male) {
+		this.name = name;
+		this.age = age;
+		this.male = male;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public boolean isMale() {
+		return male;
+	}
+
+	public void setMale(boolean male) {
+		this.male = male;
+	}
+	
+	
+}
